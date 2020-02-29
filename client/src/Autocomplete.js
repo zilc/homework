2aba5e0f62f5249d7418653af58cb65da8750da0
zilc <55707938+zilc@users.noreply.github.com>
2020-02-29 20:41:16 +0200

    Create getInfo function for getting data from movie API

--- client/src/Autocomplete.js
+++ client/src/Autocomplete.js
@@ -1,4 +1,8 @@
 import React, { Component, Fragment } from "react";
+import { debounce } from "lodash";
+
+const apiKey = "87d395aaa72bae75df666834b8e838d3";
+const hostUrl = "https://api.themoviedb.org/3";
 
 class Autocomplete extends Component {
   constructor(props) {
@@ -15,6 +19,30 @@ class Autocomplete extends Component {
       inputClicked: false
     };
   }
+
+  getInfo = debounce(() => {
+    if (this.state.userInput.length > 2) {
+      fetch(
+        `${hostUrl}/search/movie?api_key=${apiKey}&language=en-US&query=
+        ${this.state.userInput}`
+      )
+        .then(results => results.json())
+        .then(results => {
+          this.setState({
+            filteredSuggestions: results.results
+              .map(obj => ({
+                title: obj.title,
+                rating: obj.vote_average,
+                id: obj.id,
+                release_date: obj.release_date
+                  ? obj.release_date.split("-")[0]
+                  : "-"
+              }))
+              .slice(0, 8)
+          });
+        });
+    }
+  }, 150);
 }
 
 export default Autocomplete;
