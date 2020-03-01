9990c59a2ab8bc938095962dab0d399fd03ebb24
zilc <55707938+zilc@users.noreply.github.com>
2020-03-01 17:40:04 +0200

    Updates

---
+++ precache-manifest.8b18ebed734fb33364b41ce689b655a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83b293c7d94a63fff0eda4b803c405c",
+    "url": "/zilc/homework/tree/master/client/index.html"
+  },
+  {
+    "revision": "576fefd9c4fd243af2ab",
+    "url": "/zilc/homework/tree/master/client/static/css/main.96759321.chunk.css"
+  },
+  {
+    "revision": "8f97acf6557c37dfae95",
+    "url": "/zilc/homework/tree/master/client/static/js/2.c4af7f7a.chunk.js"
+  },
+  {
+    "revision": "3b173ed738774b4bb64ac2b5ddb03f3b",
+    "url": "/zilc/homework/tree/master/client/static/js/2.c4af7f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576fefd9c4fd243af2ab",
+    "url": "/zilc/homework/tree/master/client/static/js/main.06561721.chunk.js"
+  },
+  {
+    "revision": "d50048213c19b304efe1",
+    "url": "/zilc/homework/tree/master/client/static/js/runtime-main.4dac77f4.js"
+  }
+]);
